ff7417f16253356a431329c27bb6d62a28d311f1
deniz <deniz.hofmeister@ranmarine.io>
2023-11-27 17:14:41 +0100

    added catpuccin

--- nvim/lua/plugins/base.lua
+++ nvim/lua/plugins/base.lua
@@ -1,3 +1,18 @@
 if true then
-  return {}
+  return {
+    {
+      "catppuccin/nvim",
+      lazy = false,
+      name = "catppuccin",
+      config = function()
+        require("catppuccin").setup({ flavour = "macchiato" })
+      end,
+    },
+    {
+      "LazyVim/LazyVim",
+      opts = {
+        colorscheme = "catppuccin",
+      },
+    },
+  }
 end
